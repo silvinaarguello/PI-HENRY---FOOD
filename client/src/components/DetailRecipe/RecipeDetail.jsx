722c23804a421df903ac3b344e0d689808c7ede7
unknown <silvinaaguello@gmail.com>
2022-11-21 17:48:39 -0300

    falta que ande el paginado

--- client/src/components/DetailRecipe/RecipeDetail.jsx
+++ client/src/components/DetailRecipe/RecipeDetail.jsx
@@ -7,64 +7,78 @@ import s from './RecipeDetail.module.css';
 
 
 
-export default function RecipeDetail() {
-  const { id } = useParams();
-  const dispatch = useDispatch();
-  const { image, name, diet, summary, score, healthScore, instructions } =
-    useSelector((state) => state.detail);
+// export default function RecipeDetail() {
+//   const { id } = useParams();
+//   const dispatch = useDispatch();
+//   const { image, name, diet, summary, score, healthScore, instructions } =
+//     useSelector((state) => state.detail);
 
-    console.log('detail');
+//     console.log('detail');
+
+//   useEffect(() => {
+
+//     dispatch(getRecipeId(id));
+//     return () => {
+//       dispatch(cleanDetail());
+//     };
+//   }, [id, dispatch]);
+const RecipeDetail = ({match}) => {
+  let dispatch = useDispatch();
+  let id = match.params.id;
+  let p = useSelector((state) => state.detail)
 
   useEffect(() => {
-
     dispatch(getRecipeId(id));
     return () => {
       dispatch(cleanDetail());
     };
   }, [id, dispatch]);
 
+
+
   return (
     <div>
       <NavBar />
       <div className={s.content}>
-        <h3 className={s.title}>{name}</h3>
+        <h3 className={s.title}>{p.name}</h3>
         <img
-          src={image}
+          src={p.image}
           alt="Img Not Found"
           width="500px"
           className={s.img}
         />
         <div className={s.summary}>
-          <p>{summary && summary.replace(/<[^>]+>/g, "")}</p>
+          <p>{p.summary && p.summary.replace(/<[^>]+>/g, "")}</p>
         </div>
         <div className={s.types}>
-          {diet?.map((d) => (
+          {p.diet?.map((d) => (
             <h4 key={d}>{d}</h4>
           ))}
         </div>
         <div className={s.scores}>
           <span id="score">
             Score:{" "}
-            <progress id="score" max="100" value={score} className={s.score} />{" "}
-            {score}/100
+            <progress id="score" max="100" value={p.score} className={s.score} />{" "}
+            {p.score}/100
           </span>
           <span id="healthScore">
             Health Level:{" "}
             <progress
               id="healthScore"
               max="100"
-              value={healthScore}
+              value={p.healthScore}
               className={s.score}
             />{" "}
-            {healthScore}/100
+            {p.healthScore}/100
           </span>
         </div>
         <div className={s.instructions}>
-          {instructions && (
-            <p dangerouslySetInnerHTML={{ __html: `${instructions}` }} />
+          {p.instructions && (
+            <p dangerouslySetInnerHTML={{ __html: `${p.instructions}` }} />
           )}
         </div>
       </div>
     </div>
   );
 }
+ export default RecipeDetail;
